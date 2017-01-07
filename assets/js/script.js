065932d9048ebbd4d90b0231057b3f2ab7312e53
Tamas Sztanka-Toth <sztankatt@gmail.com>
2017-01-07 22:14:35 +0100

    added new links

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 /* removing height and width elements from img */
 
-jQuery(document).ready(function($){
+$(document).ready(function(){
 
 	$('img').each(function(){ 
 		$(this).removeAttr('width')
 		$(this).removeAttr('height');
 	});
+
+	$('a').filter(function() {
+	   return this.hostname && this.hostname !== location.hostname;
+	}).attr('target', '_blank');
 
 });
