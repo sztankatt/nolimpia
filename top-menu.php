f2a9cfb562dba5506adaa142ba1ad3c34333e327
marcomasik <marcomasik@gmail.com>
2017-01-09 19:00:29 +0100

    init collapsible bootstrap navbar

--- top-menu.php
+++ top-menu.php
@@ -2,43 +2,40 @@
 
 ?>
 <div id="white-header" class="header header-white">
-    <div id="logo" class="logo">
-        <a href="/">&nbsp;
-        </a>
-    </div>
-    <ul>
-        <li>
-          <a href="/nepszavazas-a-2024-budapesti-olimpia-ellen-itt-ird-ala/" class="olimpia-voros">itt írd alá</a>
-        </li>
-        <li>
-          <a href="/adomany-olimpia-nepszavazas-budapest-2024/" class="olimpia-zold">támogass</a>
-        </li>
-        <li>
-          <a href="/nepszavazas-2024-budapest-olimpia-csatlakozz-aktivista/" class="olimpia-sarga">vegyél részt</a>
-
-        </li>
-        <li>
-            <a href="#" class="momentum-lila-hover" data-toggle="modal" data-target="#momentum-desc">momentum</a>
-        </li>
-        <li>
-          <a href="/kontakt-nolimpia-momentum-nepszavazas-olimpia-2024/">kapcsolat</a>
-        </li>
-
-        <li>
-            <a target="_blank" href="https://www.facebook.com/nolimpiabp"><img style="margin-left:0px;" src="<?php echo get_theme_file_uri('assets/img/ikonok-001-01.png'); ?>"></img>
+    <nav class="navbar navbar-inverse navbar-static-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span> 
+          </button>
+          <a class="navbar-brand" href="#">
+            <div id="logo" class="logo">
+                 <a href="/">&nbsp;
+                 </a>
+            </div>
             </a>
-        </li>
-        <li>
-            <a  target="_blank" href="https://www.twitter.com/nolimpiabp"><img src="<?php echo get_theme_file_uri('assets/img/ikonok-001-02.png'); ?>"></img>
-            </a>
-        </li>
-        <li>
-            <a target="_blank" href="https://www.youtube.com/nolimpiabp"><img src="<?php echo get_theme_file_uri('assets/img/ikonok-001-03.png'); ?>"></img>
-            </a>
-        </li>
-        <li>
-            <a target="_blank" href="https://www.instagram.com/nolimpiabp"><img src="<?php echo get_theme_file_uri('assets/img/ikonok-001-04.png'); ?>"></img>
-            </a>
-        </li>
-    </ul>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li><a href="/nepszavazas-a-2024-budapesti-olimpia-ellen-itt-ird-ala/" class="olimpia-voros">itt írd alá</a></li>
+                <li><a href="/adomany-olimpia-nepszavazas-budapest-2024/" class="olimpia-zold">támogass</a></li> 
+                <li><a href="/nepszavazas-2024-budapest-olimpia-csatlakozz-aktivista/" class="olimpia-sarga">vegyél részt</a></li>
+                <li><a href="#" class="momentum-lila-hover" data-toggle="modal" data-target="#momentum-desc">momentum</a></li>
+                <li><a href="/kontakt-nolimpia-momentum-nepszavazas-olimpia-2024/">kapcsolat</a></li>
+                <li><a target="_blank" href="https://www.facebook.com/nolimpiabp"><img style="margin-left:0px;" src="<?php echo get_theme_file_uri('assets/img/ikonok-001-01.png'); ?>"></img>
+                </a></li>
+                <li><a  target="_blank" href="https://www.twitter.com/nolimpiabp"><img src="<?php echo get_theme_file_uri('assets/img/ikonok-001-02.png'); ?>"></img>
+                </a></li>
+                <li><a target="_blank" href="https://www.youtube.com/nolimpiabp"><img src="<?php echo get_theme_file_uri('assets/img/ikonok-001-03.png'); ?>"></img>
+                </a></li>
+                <li><a target="_blank" href="https://www.instagram.com/nolimpiabp"><img src="<?php echo get_theme_file_uri('assets/img/ikonok-001-04.png'); ?>"></img>
+                </a></li>
+              </ul>
+          <ul class="nav navbar-nav navbar-right">
+            </ul>
+        </div>
+      </div>
+    </nav>
 </div>
