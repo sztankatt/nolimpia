03b5402b906dea9dc0ababd450e403bc7f22382d
Tamas Sztanka-Toth <sztankatt@gmail.com>
2017-01-17 12:27:43 +0000

    last try

--- page-landing.php
+++ page-landing.php
@@ -18,13 +18,18 @@ Template Name: Landing page
     <div class="top-page" style="background-image: url('<?php echo $background ?>');">
         <div class="landing-container">
 			<div class="slogan">NEM AZ OLIMPIÁRA, IGEN A JÖVŐNKRE!</div>
-      <div class="row video-row">
-                <div class="col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6 col-lg-offset-4 col-lg-4">
-                    <div class="iframe-container">
-                        <iframe width="560" height="315" src="https://www.youtube.com/embed/PN2iHd7c_As" frameborder="0" allowfullscreen></iframe>
-                    </div>
-                </div>
-            </div>
+      <div class="row">
+        <div class="col-lg-10 col-lg-offset-1">
+          <div class="row video-row">
+          <div class="col-xs-offset-1 col-xs-10 col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6">
+              <div class="iframe-container">
+                  <iframe width="560" height="315" src="https://www.youtube.com/embed/PN2iHd7c_As" frameborder="0" allowfullscreen></iframe>
+              </div>
+          </div>
+      </div>
+
+        </div>
+      </div>
 			<div class="row">
         <div class="col-xs-10 col-xs-offset-1 col-md-8 col-md-offset-2 col-lg-offset-3 col-lg-6">
           <div class="row">
