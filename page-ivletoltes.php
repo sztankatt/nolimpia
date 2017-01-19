9938f1e04bf6be72c83af512f17c5cc7518d91a8
Tamas Sztanka-Toth <sztankatt@gmail.com>
2017-01-19 10:33:09 +0000

    fixed zip error

--- page-ivletoltes.php
+++ page-ivletoltes.php
@@ -8,7 +8,7 @@ if($_POST["ivletoltes_checkbox"] == true){
 
 		            header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
 		            header("Cache-Control: public"); // needed for internet explorer
-		            header("Content-Type: application/zip");
+		            header("Content-Type: application/pdf");
 		            header("Content-Transfer-Encoding: Binary");
 		            header("Content-Length:".filesize($attachment_location));
 		            header("Content-Disposition: attachment; filename=alairasgyujto_iv.pdf");
